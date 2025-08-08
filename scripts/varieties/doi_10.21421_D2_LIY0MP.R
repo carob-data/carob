9ee524d09591d6614b8b74808371789ac1454f0d
rhijmans <r.hijmans@gmail.com>
2025-08-07 23:28:02 -0700

    for #666

--- scripts/varieties/doi_10.21421_D2_LIY0MP.R
+++ scripts/varieties/doi_10.21421_D2_LIY0MP.R
@@ -3,12 +3,17 @@
 
 
 carob_script <- function(path) {
-  
-  "Evaluation of medium duration lines for agronomic traits and Foliar diseases under post rainy season in Buk"
-  
+   
+"
+Evaluation of medium duration lines for agronomic traits and Foliar diseases under post rainy season in Buk
+
+Medium-Duration Multi-location trial conducted under Post-season .The trial was aimed to test their adaptability, yield superiority, resistance and tolerance to biotic and abiotic stresses across the location which includes, Buk
+"
+
   uri <- "doi:10.21421/D2/LIY0MP"
   group <- "varieties"
   ff  <- carobiner::get_data(uri, path, group)
+
   meta <- carobiner::get_metadata(uri, path, group, major=1, minor=0,
               data_organization = "ICRISAT",                                  
               publication = NA,
@@ -19,7 +24,7 @@ carob_script <- function(path) {
               completion = 100,
               carob_contributor = "Gacheri Nturibi",
               carob_date = "2025-08-07",
-              notes = "All variables denoted as dates, (DEM, DFF and DM) in the metadata were treated as days",
+              notes = NA,
               design = NA
   )
   
@@ -36,11 +41,10 @@ carob_script <- function(path) {
     dmy_residue = r$DFWkgha,
     seed_weight = as.numeric(r$HSW)*10, # From 100 to 1000 Seed Weight
     shelling_percentage = r$`Shelling ratio`,
-    maturity_days = r$DM,
-    diseases = "early leaf spot;late leaf spot; rust",
-    disease_severity = apply(r[, c("ELS", "LLS", "RUST")], 1, \(i) paste0(i, collapse=";")),
-    severity_scale = "1-9"
+    maturity_days = r$DM
   )
+  
+  d$planting_date <- "2015-06-14" # from dataverse metadata 
   
   d$trial_id <- "1"
   d$crop <- "groundnut"
@@ -48,17 +52,26 @@ carob_script <- function(path) {
   d$country <- "Nigeria"
   d$adm1 <- "Kano"
   
-  d$location <- "BUK (Bayero University, Kano)" 
-  d$longitude <- mean(c(8.474923, 8.475408))
-  d$latitude <- mean(c(11.983994, 11.983964))
-  d$geo_from_source <- TRUE
+  d$location <- "Kano"
+  d$site <- "Bayero University"
+
+  d$longitude <- 8.4752
+  d$latitude <- 11.9840
+  d$geo_from_source <- FALSE
   
   d$on_farm <- NA
   d$is_survey <- FALSE
   d$irrigated <- NA
   d$N_fertilizer <- d$P_fertilizer <- d$K_fertilizer <- as.numeric(NA)
-  
-  d$planting_date <- "2015"
-  
-  carobiner::write_files(path, meta, d)
+    
+	
+	d$record_id <- 1:nrow(d)
+
+	x <- reshape(r[, c("ELS", "LLS", "RUST")], direction="long", varying=c("ELS", "LLS", "RUST"), v.names="disease_severity", timevar="diseases")
+	x$diseases <- c("early leaf spot", "late leaf spot", "rust")[x$diseases]
+	colnames(x)[3] <- "record_id"
+	x$severity_scale <- "1-9"
+	x$disease_severity <- as.character(x$disease_severity)
+		
+  carobiner::write_files(path, meta, d, long=x)
 }
