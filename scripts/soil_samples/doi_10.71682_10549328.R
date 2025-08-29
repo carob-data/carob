a0824248e54062de91130c16724a873800c7a8b0
rhijmans <r.hijmans@gmail.com>
2025-08-29 04:53:19 -0700

    for #699

--- scripts/soil_samples/doi_10.71682_10549328.R
+++ scripts/soil_samples/doi_10.71682_10549328.R
@@ -33,37 +33,42 @@ Selected soil properties were predicted from 232 topsoil samples subjected to sp
 	r <- read.csv(f)
 
 	d <- data.frame(
-	country=r$country,
-	adm1=r$adm1,
-	adm2=r$adm2,
-	longitude=r$longitude,
-	latitude=r$latitude,
-	soil_depth=r$soil_depth,
-	depth_top=r$soil_sample_top,
-	depth_bottom=r$soil_sample_bottom,
-	soil_sand=r$Sand,
-	soil_clay=r$Clay,
-	soil_silt=r$Silt,
-	soil_texture=tolower(r$Soil_Textural_Class),
-	soil_pH=r$pH,
-	soil_SOC=r$SOC,
-	soil_N = r$TN*10000,# from % to mg/ka
-	soil_PSI=r$PSI,
-	soil_Al=r$m3.Al,
-	soil_B = r$m3.B,
-	soil_Ca = r$m3.Ca,
-	soil_Fe = r$m3.Fe,
-	soil_K = r$m3.K,
-	soil_Mg = r$m3.Mg,
-	soil_Mn = r$m3.Mn,
-	soil_Na = as.numeric(r$m3.Na),
-	soil_S = as.numeric(r$m3.S),
-	soil_ex_acidity=r$ExAc,
-	soil_CEC=r$CEC,
-	soil_method="mid-infrared (MIR) spectroscopy"
+		country=r$country,
+		adm1=r$adm1,
+		adm2=r$adm2,
+		longitude=r$longitude,
+		latitude=r$latitude,
+		soil_depth=r$soil_depth,
+		depth_top=r$soil_sample_top,
+		depth_bottom=r$soil_sample_bottom,
+		soil_sand=r$Sand,
+		soil_clay=r$Clay,
+		soil_silt=r$Silt,
+		soil_texture=tolower(r$Soil_Textural_Class),
+		soil_pH=r$pH,
+		soil_SOC=r$SOC,
+		soil_N = r$TN*10000,# from % to mg/ka
+		soil_PSI=r$PSI,
+		soil_Al=r$m3.Al,
+		soil_B = r$m3.B,
+		soil_Ca = r$m3.Ca,
+		soil_Fe = r$m3.Fe,
+		soil_K = r$m3.K,
+		soil_Mg = r$m3.Mg,
+		soil_Mn = r$m3.Mn,
+		soil_Na = as.numeric(r$m3.Na),
+		soil_S = as.numeric(r$m3.S),
+		soil_ex_acidity=r$ExAc,
+		soil_CEC=r$CEC,
 	) 
 	d$geo_from_source <- TRUE
 
-	carobiner::write_files(path, meta, d)
+
+	soilmeta <- data.frame(
+		soil_element = c("Al", "B", "Ca", "Fe", "K", "Mg", "Mn", "Na", "S"),
+		soil_method = "Mehlich3 (estimated from spectroscopy)"
+	)
+
+	carobiner::write_files(path, meta, d, soil_meta=soilmeta)
 }
 
