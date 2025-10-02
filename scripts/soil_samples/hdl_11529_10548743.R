4658a27c5dd24a73cb02516fea42a70f35b51f38
Cliffoe08 <blessingdzuda08@gmail.com>
2025-10-02 12:54:37 +0200

    corrected soil script hdl:11529/10548743

--- scripts/soil_samples/hdl_11529_10548743.R
+++ scripts/soil_samples/hdl_11529_10548743.R
@@ -81,11 +81,10 @@ Soil sampling with 1x1 km grid in the agricultural area of the Region Mixteca at
    d$soil_texture <- gsub("Arcilla","clay",d$soil_texture)
    d$soil_texture <- gsub("Franco","loam",d$soil_texture)
    d$soil_texture <- gsub("Arena","sand",d$soil_texture)
-   #d$soil_texture <- gsub(".",NA,d$soil_texture)
-   
+
    soilmeta <- data.frame(
      variable = c("soil_Al", "soil_B", "soil_Ca", "soil_Fe", "soil_K", "soil_Mg", "soil_Mn", "soil_Na", "soil_S", "soil_Zn", "soil_Cu","soil_N"),
-     method = c("Mehlich3")
+     method = c("Potassium Chloride","Ammonium Acetate","Ammonium Acetate","DTPA+Sorbitol pH 7","Ammonium Acetate","Ammonium Acetate","DTPA+Sorbitol pH 7","Ammonium Acetate","Turbidimetric","DTPA+Sorbitol pH 7","DTPA+Sorbitol pH 7","Salicylic Acid Nitration")
    )
    
   carobiner::write_files(path, meta, d)
