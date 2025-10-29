b3ef7ac594d183629e8d93397d4e698f23eca2f8
rhijmans <r.hijmans@gmail.com>
2025-10-28 18:25:14 -0700

    for #756

--- scripts/agronomy/hdl_11529_10548812.R
+++ scripts/agronomy/hdl_11529_10548812.R
@@ -33,10 +33,9 @@ This experiments were established with different rates of nitrogen in order to g
 	f <- ff[basename(ff) == "GreenSeeker Oaxaca 2016-2019.xlsx"]
  
 #Function to standardize other sheets with same structure
-	make_standard_df <- function(file, sheet_name) {
-		r <- carobiner::read.excel.hdr(file, sheet = sheet_name, hdr=1, skip=1, fix_names=TRUE)
-		main<- data.frame(
-		  record_id = seq_len(nrow(r)),
+	make_standard_df <- function(sheet_name) {
+		r <- carobiner::read.excel.hdr(f, sheet=sheet_name, hdr=1, skip=1, fix_names=TRUE)
+		data.frame(
 		  country = "Mexico",
 		  adm2 = r$Municipality,
 		  location = r$Locality,
@@ -50,28 +49,17 @@ This experiments were established with different rates of nitrogen in order to g
 		  rep = as.integer(r$Inf.Experiment_REP),
 		  N_fertilizer = r$Rate.N.kg.ha,
 		  yield =  r$Yield.at.14pct.hum,
-		  plot_area = r$Plot.size.m2
+		  plot_area = r$Plot.size.m2,
+		  sheet_id = sheet_name,
+		  row_id = seq_len(nrow(r))
 		)
 	}
 	
-	#Loading all sheets and creating sheet_id
-	x1 <- make_standard_df(f, "2016")
-	x1$sheet_id <- 1
-	x2 <- make_standard_df(f, "2017")
-	x2$sheet_id <- 2
-	x3 <- make_standard_df(f, "2018")
-	x3$sheet_id <- 3
-	x4 <- make_standard_df(f, "2019")
-	x4$sheet_id <- 4
+	# reading all sheets
+	years <- as.character(2016:2019)
+	d <- do.call(rbind, lapply(years, make_standard_df))
 	
-	#unique_id for merging
-	x1$unique_id <- paste(x1$sheet_id, x1$record_id, sep = "_")
-	x2$unique_id <- paste(x2$sheet_id, x2$record_id, sep = "_")
-	x3$unique_id <- paste(x3$sheet_id, x3$record_id, sep = "_")
-	x4$unique_id <- paste(x4$sheet_id, x4$record_id, sep = "_")
-	
-	d <-rbind(x1,x2,x3,x4)
-	
+
 	# INIFAP Sitio Experimental Mixteca, Santo Domingo Yanhuitlán 
 	d$latitude <- 17.511  
 	d$longitude <- -97.352
@@ -84,68 +72,33 @@ This experiments were established with different rates of nitrogen in order to g
 	d$P_fertilizer <- d$K_fertilizer <- d$S_fertilizer <- d$lime <- as.numeric(NA)
 	d$yield_part <- "grain"
 	d$yield_moisture <- 14
-	d$land_prep_method <- gsub("conservation","minimum tillage", d$land_prep_method)
-	d$land_prep_method <- gsub("bed","raised beds", d$land_prep_method)
+	d$land_prep_method <- gsub("conservation", "minimum tillage", d$land_prep_method)
+	d$land_prep_method <- gsub("bed", "raised beds", d$land_prep_method)
+
+	d$record_id <- 1:nrow(d)
 	
-##########
-#NDVI_data
-##########
-	standardize_ndvi <- function(file, sheetname) {
-	  all_data <- list()
-	  
-	  for (sheet in sheetname) {
-	    
-	    df <- readxl::read_excel(file, sheet = sheet, skip = 1)
-	    ndvi_cols <- grep("date\\.|ndvi", names(df), ignore.case = TRUE)
-	    df <- df[, ndvi_cols]
-	    
-	    n_meas <- ncol(df) / 2
-	    long_list <- list()
-	    
-	    for (i in seq_len(n_meas)) {
-	      date_col <- df[[ (i - 1) * 2 + 1 ]]   
-	      ndvi_col <- df[[ (i - 1) * 2 + 2 ]]   
-	      
-	      meas_name <- paste0("Measurement_", i)
-	      ndvi_col <- as.numeric(ndvi_col)
-	      temp <- data.frame(
-	        record_id = seq_len(nrow(df)),
-	        year = sheet,
-	        variable = meas_name,
-	        date = date_col,
-	        value = ndvi_col,     
-	        stringsAsFactors = FALSE
-	      )
-	      
-	      temp <- temp[!is.na(temp$value), ]
-	      long_list[[i]] <- temp
-	    }
-	    
-	    sheet_long <- do.call(rbind, long_list)
-	    all_data[[sheet]] <- sheet_long
-	  }
-	  
-	  final_long <- do.call(rbind, all_data)
-	  return(final_long)
-	}
-	
-	n1 <- standardize_ndvi(f,"2016")
-	n1$sheet_id <- 1
-	n2 <- standardize_ndvi(f,"2017")
-	n2$sheet_id <- 2
-	n3 <- standardize_ndvi(f,"2018")
-	n3$sheet_id <- 3
-	n4 <- standardize_ndvi(f,"2019")
-	n4$sheet_id <- 4
-	
-	n1$unique_id <- paste(n1$sheet_id, n1$record_id, sep = "_")
-	n2$unique_id <- paste(n2$sheet_id, n2$record_id, sep = "_")
-	n3$unique_id <- paste(n3$sheet_id, n3$record_id, sep = "_")
-	n4$unique_id <- paste(n4$sheet_id, n4$record_id, sep = "_")
-	
-	NDVI_data <- rbind(n1,n2,n3,n4)
-	
-	#d <- merge(d,NDVI_data,by ="unique_id")
-	
-	carobiner::write_files(path, meta, d)
+####################
+# NDVI_data
+
+## reading the same sheets as above again. This is not efficient, but it helps keep the code simple
+	standardize_ndvi <- function(sheetname) { 
+		df <- carobiner::read.excel.hdr(f, sheet=sheetname, hdr=1, skip=1, fix_names=TRUE, na="-")
+	    ndvi_cols <- grep("ndvi", names(df), ignore.case = TRUE)
+		date_cols <- grep("Measure.*.date", names(df), ignore.case = TRUE)
+		data.frame(
+			sheet_id = sheetname,
+			row_id = 1:nrow(df),
+			date=as.vector(as.matrix(df[, date_cols])), 
+			NDVI=as.numeric(as.vector(as.matrix(df[, ndvi_cols])))
+		)
+	 }
+	NDVI <- do.call(rbind, lapply(years, standardize_ndvi))
+
+	NDVI <- merge(NDVI, d[, c("record_id", "sheet_id", "row_id")], by=c("sheet_id", "row_id"))
+	NDVI <- NDVI[order(NDVI$record_id, NDVI$date), ]
+
+	NDVI$row_id <- NDVI$sheet_id <- NULL
+	d$row_id <- d$sheet_id <- NULL
+
+	carobiner::write_files(path, meta, d, long=NDVI)
 }
