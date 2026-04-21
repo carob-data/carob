c3df8a2ea63323c2fe66ff51a59be0ac6e04d320
rhijmans <r.hijmans@gmail.com>
2026-04-21 07:08:47 -0700

    for #837

--- scripts/survey/doi_10.7910_DVN_TLKYUA.R
+++ scripts/survey/doi_10.7910_DVN_TLKYUA.R
@@ -32,16 +32,12 @@ Ghana Africa RISING follow-up evaluation survey was implemented in 2020. The sur
 		notes = "We process only files with useful information for carob"
 	)
 	
-
-	f1 <- ff[basename(ff) == "Community"]
-	f2 <- ff[basename(ff) == "Household"]
-
-	r1 <- haven::read_dta(paste(f2, "Data/Interview.dta", sep = "/")) |> carobiner:::unlabel()
-	r2 <- haven::read_dta(paste(f2, "Data/Section B.dta", sep = "/")) |> carobiner:::unlabel()
-	r3 <- haven::read_dta(paste(f2, "Data/Section E.dta", sep = "/")) |> carobiner:::unlabel()
-	r4 <- haven::read_dta(paste(f2, "Data/Section G1.dta", sep = "/")) |> carobiner:::unlabel()
-	r5 <- haven::read_dta(paste(f2, "Data/Section G2.dta", sep = "/")) |> carobiner:::unlabel()
-	r6 <- haven::read_dta(paste(f2, "Data/Section H.dta", sep = "/")) |> carobiner:::unlabel()
+	r1 <- haven::read_dta(grep("Data/Interview.dta", ff, value=TRUE)) |> carobiner:::unlabel()
+	r2 <- haven::read_dta(grep("Data/Section B.dta", ff, value=TRUE)) |> carobiner:::unlabel()
+	r3 <- haven::read_dta(grep("Data/Section E.dta", ff, value=TRUE)) |> carobiner:::unlabel()
+	r4 <- haven::read_dta(grep("Data/Section G1.dta", ff, value=TRUE)) |> carobiner:::unlabel()
+	r5 <- haven::read_dta(grep("Data/Section G2.dta", ff, value=TRUE)) |> carobiner:::unlabel()
+	r6 <- haven::read_dta(grep("Data/Section H.dta", ff, value=TRUE)) |> carobiner:::unlabel()
 	
 	
 	#### process
