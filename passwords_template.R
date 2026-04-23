819f1effe9499b1d06e466c6d15b9d0728f8a759
rhijmans <r.hijmans@gmail.com>
2026-04-23 06:45:11 -0700

    v

--- passwords_template.R
+++ passwords_template.R
@@ -3,10 +3,12 @@
 
 pwds <- function() {
 	list(
-		LSMS = c(username="name@email.com", password="password"),
+		LSMS = list(username="name@email.com", password="password"),
 		## DRYAD "Client ID" and "Secret" see https://datadryad.org/account 
-		DRYAD = c(username="xq4xsE4lO$_((kssSSS_wCdU", password="XmJaJC__PP1x-x3EAQZvvxA"),
-		HARVARD = c(username="my name", password=NULL, institution="ZzZzZ", email="name@email.com")
+		DRYAD = list(username="xq4xsE4lO$_((kssSSS_wCdU", password="XmJaJC__PP1x-x3EAQZvvxA"),
+		## DATAVERSE: name/email/institution are used for guestbook responses; 
+		## token is the Dataverse API token (sent as X-Dataverse-key) for restricted files.
+		DATAVERSE = list(name="my name", email="name@email.com", institution="ZzZzZ", token=NULL)
 	)
 }
 
