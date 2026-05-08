533eccad3c8cc1428fe91e5873016760869bc6de
rhijmans <r.hijmans@gmail.com>
2026-05-08 00:02:24 -0700

    for #857

--- scripts/agronomy/doi_10.7910_DVN_4W2G0I.R
+++ scripts/agronomy/doi_10.7910_DVN_4W2G0I.R
@@ -3,12 +3,12 @@
 
 ## ISSUES
 #1. Higher yield recorded for beans, probably as a result of both grain and pod weight
-carob_script <- function(path) {-
+carob_script <- function(path) {
 
-  "56 bean accessions were planted in a randomised block design, with three replications. All blocks received the same treatment. The block sizes were 2m by 2m. The phenotypic data was collected using the descriptors from the International Board for Plant Genetic Resources. (2019-07-01)"
+"56 bean accessions were planted in a randomised block design, with three replications. All blocks received the same treatment. The block sizes were 2m by 2m. The phenotypic data was collected using the descriptors from the International Board for Plant Genetic Resources. (2019-07-01)"
   
   uri <- "doi:10.7910/DVN/4W2G0I"
-  group <- "agronomy"
+  group <- "varieties"
   ff  <- carobiner::get_data(uri, path, group)
   
   meta <- carobiner::get_metadata(uri, path, group, major=1, minor=0,
@@ -26,23 +26,8 @@ carob_script <- function(path) {-
   )
   
   f <- ff[basename(ff) == "Hoima beans onstation.xlsx"]
-  
-  #fixing column names first
-  f1 <- carobiner::read.excel(
-    f, n_max = 2, col_names = FALSE)
-  
-  col_names <- apply(f1, 2, function(x) {
-    x <- x[!is.na(x) & x != ""]
-    paste(x, collapse = "_")})
-  
-  #readng actual data
-  r <- carobiner::read.excel(f, skip = 3, col_names = FALSE)
-  
-  names(r) <- col_names
-  
-  r <- r[-c(1,2), ]
+  r <- carobiner::read.excel.hdr(f, skip=4, hdr=2)
 
-  
   d <- data.frame(
     country = "Uganda",
     adm2="Hoima",
@@ -50,13 +35,14 @@ carob_script <- function(path) {-
     latitude=1.43,
     longitude=31.35,
     crop="common bean",
-    treatment=r$`Name of variety`,
-    variety=r$`Name of variety`,
-    emergence_days=as.integer(r$`Days from sowing_to germination`),
-    flowering_days=as.integer(r$`Days from sowing_to flowering`),
-    seed_weight=as.numeric(r$`100% seed_weight (g)`),
+    treatment=r$Name.of.variety,
+    variety=r$Name.of.variety,
+    emergence_days=as.integer(r$Days.from.sowing_to.germination),
+    flowering_days=as.integer(r$Days.from.sowing_to.flowering),
+    seed_weight=as.numeric(r$X100pct.seed_weight.g),
     yield=as.numeric(r$Total_Kgs)*2500,#converting to kg/ha since weight recorded was from 2m by 2m plots
-    yield_part="grain")
+    yield_part="grain"
+  )
     
     
 
@@ -67,6 +53,7 @@ carob_script <- function(path) {-
     d$geo_from_source <- FALSE
     d$P_fertilizer <- d$K_fertilizer <- d$N_fertilizer <- as.numeric(NA)
     d$yield_moisture <- as.numeric(NA)
+    d$yield_isfresh <- TRUE # seems a reasonable assumption here
 
-  carobiner::write_files(path, meta, d)
+    carobiner::write_files(path, meta, d)
 }
