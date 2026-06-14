da8075bdf8ea5e8922107ddabfc85480b2704edf
rhijmans <r.hijmans@gmail.com>
2026-06-14 09:45:30 -0700

    fixes #890

--- scripts/agronomy/doi_10.7910_DVN_ZGXBMT..R
+++ scripts/agronomy/doi_10.7910_DVN_ZGXBMT..R
@@ -2,8 +2,10 @@
 # license: GPL (>=3)
 
 ## ISSUES
-#1.NAs in NPK fertilizer columns arise from the merge for crop data that has no fertilizer data 
-#2.need to add soil_loam in carob
+
+## There are 10 treatments idenfified with a code. It is not specified what these refer to, making it impossible to interpret the data 
+## could possibly be clarified by contacting the authors
+
 carob_script <- function(path) {
 
 "
@@ -25,11 +27,12 @@ This dataset contains information of experiments carried out beans in two region
 		carob_date = "2026-06-13",
 		design = NA,
 		data_type = "on-farm experiment",
-		treatment_vars = "N_fertilizer;P_fertilizer;K_fertilizer",
+		treatment_vars = "N_fertilizer;P_fertilizer;K_fertilizer;Zn_fertilizer;B_fertilizer;Mg_fertilizer",
 		response_vars = "yield", 
 		carob_contributor = "Blessing Dzuda",
 		completion = 100,	
-		notes = NA)
+		notes = NA
+	)
 	
 
 	f1 <- ff[basename(ff) == "01. Codebook.xls"]
@@ -49,28 +52,30 @@ This dataset contains information of experiments carried out beans in two region
 	  id=tolower(r2$id_banco),
 	  soil_pH=r2$pH,
 	  soil_SOM=r2$MO,
-	  N_fertilizer=r2$N,
-	  P_fertilizer=r2$P,
-	  K_fertilizer=r2$K,
-	  Ca_fertilizer=r2$Ca,
-	  Mg_fertilizer=r2$Mg,
-	  Fe_fertilizer=r2$Fe,
-	  Cu_fertilizer=r2$Cu,
-	  Zn_fertilizer=r2$Zn,
-	  Mn_fertilizer=r2$Mn,
+	  soil_N=r2$N,
+	  soil_P=r2$P,
+	  soil_K=r2$K,
+	  soil_Ca=r2$Ca,
+	  soil_Mg=r2$Mg,
+	  soil_Fe=r2$Fe,
+	  soil_Cu=r2$Cu,
+	  soil_Zn=r2$Zn,
+	  soil_Mn=r2$Mn,
 	  soil_ex_acidity=r2$Ac_in,
 	  soil_bd=r2$Da,
 	  soil_clay=r2$Arcilla,
-	  #soil_loam=r2$Limo,
+	  soil_silt=r2$Limo,
 	  soil_sand=r2$Arena,
-	  soil_texture=r2$Textura)
+	  soil_texture=r2$Textura
+	)
 	
 	
-	r4$npta <- NULL#removing columns before binding(variables not in carob)
-	r4$vpta <- NULL
-	r5 <- rbind(r3,r4)
+	#removing columns before binding
+	r5 <- carobiner::bindr(r3,r4)
 	
-	data <- data.frame(
+	d <- data.frame(
+		# treatment has codes   1  2  3  4  5  6  7  8  9 10
+	  treatment = r5$tto,
 	   country="Nicaragua",
 	   adm1=r5$departamento,
 	  adm2=r5$municipio,
@@ -79,22 +84,28 @@ This dataset contains information of experiments carried out beans in two region
 	  rep=as.integer(r5$rep),
 	  crop="common bean",
 	  yield=r5$rto_grano_kgha,
-	  dmy_total=r5$rto_biom_kgha#assuming its dry because common bean is harvested when the plant/pods are dry
+	  # assuming its dry because common bean is harvested when the plant/pods are dry
+	  dmy_total=r5$rto_biom_kgha,
+	  plant_density = r5$npta * 10000
 	)
+
+	da$pod_density <- 10000 * r5$vpta / r5$npta
+	d[cols] <- lapply(d[cols], \(x) carobiner::fix_name(x, case="title"))
 	
 	cols <- c("adm1", "adm2", "location", "id")
 	
-	data[cols] <- lapply(data[cols], tolower)
+	d[cols] <- lapply(d[cols], tolower)
+	
+	#### the soils do not appear to mac
 	soil[cols] <- lapply(soil[cols], tolower)
-	soil$adm2[soil$adm2 == "posotega"]     <- "posoltega"
+	soil$adm2[soil$adm2 == "posotega"] <- "posoltega"
 	
-	d <- merge(data, soil, by = cols, all.x = TRUE)#some rows in soil were discarded after the merge because
+	# some rows in soil were discarded after the merge because
 	# we only want data that corresponds to sites where the trial was done
-	d[cols] <- lapply(d[cols], tools::toTitleCase)
-	
+	d <- merge(d, soil, by = cols, all.x = TRUE)
   	
-  d$planting_date <- "2019"
-  d$trial_id <- paste(d$planting_date,d$location,sep = "_")
+	d$planting_date <- "2019"
+	d$trial_id <- paste(d$planting_date,d$location,sep = "_")
 	d$on_farm <- TRUE
 	d$is_survey <- FALSE
 	d$irrigated <- FALSE
@@ -106,15 +117,15 @@ This dataset contains information of experiments carried out beans in two region
 	  longitude = c(-86.197, -86.183, -86.303, -86.604, -84.609, -85.423)
 	)
 	
-	d <- merge(d,loc,by="location", all.x = T)
-	 d$soil_texture[d$soil_texture=="Franco Arcilloso"] <- "clay loam"
-	 d$soil_texture[d$soil_texture=="Arcilloso"] <- "clay"
+	d <- merge(d, loc,by="location", all.x = T)
+	d$soil_texture[d$soil_texture=="Franco Arcilloso"] <- "clay loam"
+	d$soil_texture[d$soil_texture=="Arcilloso"] <- "clay"
 	 
-   d$inoculated <- FALSE
+	d$inoculated <- FALSE
 	d$yield_part <- "seed"
 	d$yield_moisture <- as.numeric(NA)
-  d$yield_isfresh <- TRUE
-  d$id=NULL
+	d$yield_isfresh <- TRUE
+	d$id=NULL
   
 	carobiner::write_files(path, meta, d)
 }
