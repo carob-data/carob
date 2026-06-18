0ce362fc6ad4a935fe797be83212d97dc88ad0d1
cedricngakou <cedric.ngakou@aims-cameroon.org>
2026-06-18 17:04:01 +0100

    review

--- scripts/agronomy/doi_10.25502_s67m-gx20_d.R
+++ scripts/agronomy/doi_10.25502_s67m-gx20_d.R
@@ -15,35 +15,36 @@ carob_script <- function(path) {
 	ff  <- carobiner::get_data(uri, path, group)
 
 	meta <- carobiner::get_metadata(uri, path, group, major=NA, minor=NA,
-		data_organization = "IITA;ACAI",
+		data_organization = "IITA",
 		publication = "doi:10.1016/j.eja.2021.126242",
-		project = NA,
+		project = "ACAI",
 		carob_date = "2026-06-17",
 		design = NA,
 		data_type = "on-farm experiment",
-		treatment_vars = "N_fertilizer;P_fertilizer;K_fertilizer",
+		treatment_vars = "K_fertilizer",
 		response_vars = "none", 
 		carob_contributor = "Mitchelle Njukuya",
-		completion = 100,	
+		carob_completion = 100,	
 		carob_effort = NA,	
 		notes = NA
 	)
 	
 
-	f <- ff[basename(ff) == "fpar-selected-treatments-benue-edo-2016-and-2017.csv"]
+	f1 <- ff[basename(ff) == "fpar-selected-treatments-benue-edo-2016-and-2017.csv"]
 	f2 <- ff[basename(ff) == "treatment.csv"]
 	#f3 <- ff[basename(ff) == "column_dictionary_fpar_selectedtreatments_2016_2017.csv"]
 
-	r <- read.csv(f)
+	r1 <- read.csv(f1)
 	r2 <- read.csv(f2)
 	#r3 <- read.csv(f3)
 
 	d1 <- data.frame(
-		country = r$Country,
-		year = r$Year,
-		location = r$Loc,
-		treatment = r$Treatment,
-		photosynthetic_active_radiation = r$fPAR,
+		country = r1$Country,
+		year = r1$Year,
+		location = r1$Loc,
+		treatment = r1$Treatment,
+		PAR = r1$fPAR, # photosynthetic active radiation
+		DAP = r1$DAP,
 		crop = "cassava",                         #from publication
 		plant_density = 12500,
 		variety = "TME 419"
@@ -64,7 +65,7 @@ carob_script <- function(path) {
 	
 	d$N_splits <- as.integer(3)
 	d$fertilizer_type <- "urea;KCl;TSP"
-
+  
 	d$on_farm <- TRUE
 	d$is_survey <- FALSE
 	d$irrigated <- FALSE
@@ -86,7 +87,8 @@ carob_script <- function(path) {
 	d$longitude[d$year=="2017" & d$location=="Benue"] <- 8.19
 	d$latitude[d$year=="2017" & d$location=="Benue"] <-7.27
 	d$geo_from_source <- FALSE
-
+  d$year <- NULL 
+  
 	yield <- NA
 	d$yield_part <- "tubers"
 	d$yield_moisture <- as.numeric(NA)
