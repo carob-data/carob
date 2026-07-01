3ae2ec4461e2986761069156b3295149f7ac5377
rhijmans <r.hijmans@gmail.com>
2026-07-01 09:16:11 -0700

    for #935

--- scripts/agronomy/doi_10.7910_DVN_GT9OC4.R
+++ scripts/agronomy/doi_10.7910_DVN_GT9OC4.R
@@ -2,7 +2,7 @@
 # license: GPL (>=3)
 
 ## ISSUES
-#NA values on yield
+
 
 carob_script <- function(path) {
 
@@ -28,22 +28,24 @@ Data on agronomic traits of maturity, plant height, grain yield, resistance/tole
 		carob_contributor = "Premrose Masunungure",
 		carob_date = "2026-07-01",
 		carob_completion = 0,	
-		carob_effort = 5)
-	
+		carob_effort = 5
+	)
 
 	f <- ff[basename(ff) == "Red sorghum hybrids at Shiraro 2014.xlsx"]
-
 
 	r <- carobiner::read.excel(f, sheet="Sheet1")
 	d <- data.frame(
 	  country = "Ethiopia",
-		adm1 = "Tigrai",
-		adm2 = "Western Tigrai",
-		adm3 = r$Site,
+		adm1 = "Tigray",
+		adm2 = "Western Tigray",
+		location = r$Site,
 		treatment = r$Genotype,
 		rep = as.integer(r$Replicate),
+		planting_date = as.character(as.Date(r$Sown)),
+		harvest_date = as.character(as.Date(r$DateHarvest)),
 		plant_height = r$PHTMean,
 		yield = r$`YieldKg/Ha`,
+		variety = r$Genotype,
 		variety_pedigree = r$Pedigree,
 		plot_id = as.character(r$Plot),
 		flowering_days = r$DTF,
@@ -53,32 +55,27 @@ Data on agronomic traits of maturity, plant height, grain yield, resistance/tole
 		disease_severity = as.character(r$DiseaseScore),
 		pest_severity = as.character(r$InsectScore),
 		drought_stress = as.character(r$DroughtScore),
+		drought_stress = as.character(r$DroughtScore),
+		plant_density = r$StandAtHarv / r$PlotArea,
+		head_density = r$`Heads/Plot` / r$PlotArea,
 		crop = "sorghum"
 	)
 	
-	d$trial_id <- as.character(as.integer(as.factor( paste(d$adm3,d$planting_date,sep = "_"))))
+	d$trial_id <- "1"
 	
-	d$on_farm <- TRUE
+	d$on_farm <- TRUE # how do you know?
 	d$is_survey <- FALSE
-	d$irrigated <- FALSE
+	d$irrigated <- FALSE # how do you know?
 	
 	d$longitude <-  37.555
 	d$latitude <-  14.240
 	d$geo_from_source <- FALSE
 
-	
-	d$planting_date <- as.character(as.Date("2014-07-16"))
-	d$harvest_date  <- as.character(as.Date("2014-11-26"))
+	d$P_fertilizer <- d$K_fertilizer <- d$N_fertilizer <- NA
 
-   d$P_fertilizer <- NA
-   d$K_fertilizer <- NA
-   d$N_fertilizer <- NA
-   d$fertilizer_type <- NA
-
-   yield = r$`YieldKg/Ha`
 	d$yield_part <- "grain"
 	d$yield_moisture <- as.numeric(NA)
-	d$yield_isfresh <- TRUE
+	d$yield_isfresh <- TRUE  # how do you know?
 
 	carobiner::write_files(path, meta, d)
 }
