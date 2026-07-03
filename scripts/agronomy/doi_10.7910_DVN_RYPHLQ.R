1785b7c2b2d30833aa4ec404723aa6e7d4d9a363
primasunungure-arch <primasunungure@gmail.com>
2026-07-03 18:12:59 +0200

    Modified agronomy script doi:10.7910/DVN/RYPHLQ

--- scripts/agronomy/doi_10.7910_DVN_RYPHLQ.R
+++ scripts/agronomy/doi_10.7910_DVN_RYPHLQ.R
@@ -2,6 +2,19 @@
 # license: GPL (>=3)
 
 ## ISSUES
+# NA values on yield
+#Locations and the geodata used, All the areas I used adm3 except, 
+#areas highlighted below where I used adm1 and adm2.
+#Akora-Idi adm1, used,
+#Sirimbana adm1 used,
+#Patty adm1 used,
+#Azzazala adm1 used,
+#Sarki Haoussa adm1 used,
+#Guidan Tawayé Saboua adm2 used,
+#Baban Kori adm2 used,
+#kampa Tegui adm2 used,
+#Baban Anné adm2 used,
+
 
 carob_script <- function(path) {
 
@@ -15,46 +28,43 @@ Data were collected from on-farm trials, conducted with farmers, aiming at devel
 	group <- "agronomy"
 	ff  <- carobiner::get_data(uri, path, group)
 
+
 	meta <- carobiner::get_metadata(uri, path, group, major=1, minor=0,
 		data_organization = "ICRISAT",
 		publication = NA,
 		project = NA,
 		design = NA,
 		data_type = "on-farm experiment",
-		treatment_vars = "fertilizer_used",
+		treatment_vars = "fertilizer_used;OM_used",
 		response_vars = "yield", 
-		notes = NA,
 		carob_contributor = "Premrose Masunungure",
 		carob_date = "2026-06-25",
-		carob_completion = 0,	
+		carob_completion = 50,
 		carob_effort = 5
 	)
 	
+
 	f1 <- ff[basename(ff) == "Data on Integrated Production System 2019 & 2020-LSIL & Collaborating projects_ Niger.xlsx"]
 	f2 <- ff[basename(ff) == "Data on Pearl millet production _2019&2020_LSIL zones_Niger.xls"]
 
-	r1 <- carobiner::read.excel(f1, sheet="Integrated Pro. System 2019")
+	r1a <- carobiner::read.excel(f1, sheet="Integrated Pro. System 2019")
+	r1b <- carobiner::read.excel(f1, sheet="Sheet1")
 	r2 <- carobiner::read.excel(f2)
 
 	d1 <- data.frame(
-		trial_id = paste0("1_", r1$`N of farmer`),
-		country = "Niger",
-		adm1 = r1$Region,
-		adm2 = r1$Departement,
-		adm3 = r1$District,
-		adm4 = r1$Village,
-		field_size = as.numeric(r1$`Field area (ha)`),
-		#treatment = r1$`Production System`,
-		treatment = r1$`Treatment Groups`,
-		fwy_residue = r1$`Pearl millet Biomass Yield (kg/ha)`,
-		yield = r1$`Pearl millet grain yield (kg/ha)`,
-		crop = "pearl millet",
-		tree_density = r1$`Number of trees/ha`,	
-		shrub_density = r1$`Number of shrubs/ha`
+	country = "Niger",
+	adm1 = r1a$Region,
+	adm2 = r1a$Departement,
+	adm3 = r1a$District,
+	adm4 = r1a$Village,
+	field_size = as.numeric(r1a$`Field area (ha)`),
+	treatment = r1a$`Production System`,
+	dmy_residue = r1a$`Pearl millet Biomass Yield (kg/ha)`,
+	yield = r1a$`Pearl millet grain yield (kg/ha)`,
+	crop = "pearl millet"
 	)
 	
   d2 <- data.frame(
-	trial_id = paste0("2_", r2$`N of farmer`),
     country = "Niger",
     adm1 = r2$Region,
     adm2 = NA,
@@ -62,69 +72,73 @@ Data were collected from on-farm trials, conducted with farmers, aiming at devel
     adm4 = r2$Village,
     field_size = NA,
     treatment = r2$`Treatment Groups/ Production System`,
-    fwy_residue = r2$`Pearl millet stover yield/ha`,
+    dmy_residue = r2$`Pearl millet stover yield/ha`,
     yield = r2$`Pearl millet grain yield/ha`,
-    crop = "pearl millet"
-  )	
+    crop = "pearl millet")	
 
-  d <- carobiner::bindr(d1, d2)
+  d <- rbind(d1, d2)
+  d <- unique(d)
   
-	d$planting_date <- "2019"
-	d$harvest_date <- "2020"
+  d$planting_date <- "2019"
+  d$harvest_date <- "2020"
+  d$trial_id <- paste(d$adm4,d$planting_date,sep = "_")
 	d$on_farm <- TRUE
 	d$is_survey <- FALSE
-	d$irrigated <- FALSE
+	d$irrigated <- NA
 	
-	d$adm1 <- gsub("Tillabéry", "Tillabéri",d$adm1)
+	
+	d$adm1 <- gsub("Tillabéri", "Tillabéry",d$adm1)
+	
+  g <- unique(d[,c("country", "adm1", "adm4")])
 
-	npk <- grepl("NPK", d$treatment)
-	man <- grepl("Manure", d$treatment)
-	d$fertilizer_used <- d$OM_used <- NA  #Farmers' practices were not specified
-	d$fertilizer_used[man] <- FALSE 
-	d$fertilizer_used[npk] <- TRUE 
-	d$OM_used[npk] <- FALSE 
-	d$OM_used[man] <- TRUE 
-
-
-	#u <- unique(d[, c("adm1", "adm2", "adm3", "adm4")])
-	#u = u[!is.na(u$adm3), ]
-	#u= u[order(u$adm1, u$adm2, u$adm3, u$adm4), ]
-	#carobiner::dfput(u)
-
-## adm4 is unique, keeping other levels for checking. adm3 can be found with adm_pointRadius
-	geo <- data.frame(
-		adm1 = c("Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Tillabéri", "Tillabéri", "Tillabéri", "Tillabéri", "Tillabéri", "Tillabéri", "Tillabéri", "Tillabéri", "Tillabéri", "Zinder", "Zinder"),
-		adm2 = c("Aguié", "Aguié", "Aguié", "Dakoro", "Dakoro", "Guidan Roumdji", "Guidan Roumdji", "Guidan Roumdji", "Guidan Roumdji", "Guidan Roumdji", "Madarounfa", "Madarounfa", "Madarounfa", "Madarounfa", "Madarounfa", "Madarounfa", "Madarounfa", "Madarounfa", "Mahahi", "Mahahi", "Mahahi", "Mayahi", "Balleyara", "Balleyara", "kollo", "kollo", "Torodi", "Torodi", "Torodi", "Torodi", "Torodi", "Magaria", "Mirriah"),
-		adm3 = c("Aguié", "Aguié", "Aguié", "Adjékoria", "Sabon Machi", "Guidan Roumdji", "Guidan Roumdji", "Guidan Sori", "Guidan Sori", "Guidan Sori", "Gabi", "Gabi", "Gabi", "Madarounfa", "Safo", "Sarkin Yamma", "Sarkin Yamma", "Sarkin Yamma", "Sarki Haoussa", "Sarki Haoussa", "Sarki Haoussa", "Sarki Haoussa", "Tagazar", "Tagazar", "Dantchiandou", "Kouré", "Torodi", "Torodi", "Torodi", "Torodi", "Torodi", "Bandé", "Gouna"),
-		adm4 = c("Baban Anné", "Grpment Peulh Hardo Harouna", "Kodaou", "Akora-Idi", "Baban Kori", "Karazomé", "Karo Sofoua", "Guidan Sori", "Koki", "Noualla", "Gabi", "Samiya Goma", "Sarkin Bindigua", "Bargaja", "Safo", "Guarin Guizo", "Guarin Makoyo", "Guidan Bawa", "Azzazala", "Guidan Tawayé Saboua", "Sarki Haoussa", "Azzazala", "Tabala", "Tabala Tondi T", "kampa Tegui", "Tchioubi", "Djioga", "Koubo", "seno Tiko", "Tchampanga", "Tiko", "Gochalo", "Kassari H")
-	)
-
-## merge with adm4 should be sufficient
-#    d <- merge(d, geo, by="adm4", all.x=TRUE)
  
+  location <- data.frame(
+    adm1 = c("Maradi",
+             "Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Maradi",
+             "Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Tillabéry",
+             "Maradi", "Maradi", "Maradi", "Tillabéry", "Tillabéry", "Maradi",
+             "Maradi", "Tillabéry", "Tillabéry", "Tillabéry", "Tillabéry",
+             "Zinder", "Zinder", "Maradi", "Maradi", "Tillabéry", "Tillabéry",
+             "Tillabéry", "Tillabéry"),
+    adm4 = c("Gabi", "Akora-Idi", "Guidan Tawayé Saboua",
+             "Karazomé", "Koki", "Guarin Makoyo", "Guidan Bawa", "Safo",
+             "Bargaja", "Sarki Haoussa", "Baban Kori", "Guarin Guizo", "Samiya Goma",
+             "Noualla", "Tabala Tondi T", "Azzazala", "Guidan Sori", "Karo Sofoua",
+             "seno Tiko", "kampa Tegui", "Sarkin Bindigua", "Grpment Peulh Hardo Harouna",
+             "Koubo", "Tchioubi", "Tabala", "Tchampanga", "Gochalo", "Kassari H",
+             "Baban Anné", "Kodaou", "Tiko", "Djioga", "Sirimbana", "Patty"),
+    latitude = c(13.126, 14.116, 14.118, 13.726, 13.602, 13.439,
+                 13.439, 13.296, 13.243, 14.116, 14.617, 13.439, 13.126, 13.602, 13.738,
+                 14.116, 13.602, 13.727, 13.122, 13.478, 13.126, 13.511, 13.122,
+                 13.330, 13.738, 13.122, 13.188, 13.489, 14.618, 13.511, 13.122,
+                 13.122, 14.184, 14.184),
+    longitude = c(7.029, 7.299, 7.620, 6.535,
+                  6.714, 6.975, 6.975, 6.943, 7.345, 7.299, 6.985, 6.975, 7.029,
+                  6.714, 2.864, 7.299, 6.714, 6.536, 1.495, 2.298, 7.029, 7.625,
+                  1.495, 2.553, 2.864, 1.495, 8.897, 9.199, 6.986, 7.625, 1.495,
+                  1.495, 2.188, 2.188)
+  )
 
-#  location <- data.frame(
-#    adm1 = c("Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Maradi", "Maradi",
-#		"Maradi", "Maradi", "Tillabéri", "Maradi", "Maradi", "Maradi", "Tillabéri", "Tillabéri", "Maradi", "Maradi", "Tillabéri", "Tillabéri", "Tillabéri", "Tillabéri", "Zinder", "Zinder", "Maradi", "Maradi", "Tillabéri", "Tillabéri", "Tillabéri", "Tillabéri",
-#	adm4 = c("Gabi", "Akora-Idi", "Guidan Tawayé Saboua", "Karazomé", "Koki", "Guarin Makoyo", "Guidan Bawa", "Safo", 
-#         "Bargaja", "Sarki Haoussa", "Baban Kori", "Guarin Guizo", "Samiya Goma", "Noualla", "Tabala Tondi T", "Azzazala", "Guidan Sori", "Karo Sofoua", "seno Tiko", "kampa Tegui", "Sarkin Bindigua", "Grpment Peulh Hardo Harouna", "Koubo", "Tchioubi", "Tabala", "Tchampanga", "Gochalo", "Kassari H", "Baban Anné", "Kodaou", "Tiko", "Djioga", "Sirimbana", "Patty"),
-#	latitude = c(13.235, 14.348, 13.81, 13.653, 13.573, 13.45, 13.87, 13.415, 14, 13.841, 13.933, 13.421, 13.266, 14.436, 12.73, 
-#             13.819, 13.529, 13.63, 14.215, 13.443, 13.245, 13.491, 13.1,  13.3, 13.756, 14.212, 13.275, 13.801, 13.51, 13.465, 13.118, 
-#             13.116, 13.098, 14.212),
-#	longitude = c(7.068, 6.656, 7.494, 6.868, 6.788, 6.967, 7.285, 7.113, 7.85, 7.592, 6.966, 7.024, 7.033, 
-#              6.92, 2.012, 7.617, 6.898, 6.619, 1.455, 2.649, 7.027, 7.096,    1.816, 2.628, 3.022, 1.455, 8.771, 8.985, 7.95, 7.768, 1.799, 1.75, 1.796, 1.453)
-#	)
   
-#    d <- merge(d,location,by=c("adm1", "adm4"),all.x=TRUE)
+  d <- merge(d,location,by=c("adm1","adm4"),all.x=TRUE)
   
+  d$geo_from_source <- FALSE
+  
+  d$fertilizer_used <-NA
+  d$fertilizer_used[d$treatment%in% c("Farmers practice","Use of manure","Manure","Farmers'practices")] <- FALSE
+  d$fertilizer_used[d$treatment%in% c("Use of manure+Fertilizer (NPK)","Manure +Fertilizer (NPK)")] <- TRUE
+  
+  d$OM_used <-NA
+  d$OM_used[d$treatment%in% c("Use of manure", "Manure", "Manure +Fertilizer (NPK)", "Use of manure+Fertilizer (NPK)")] <- TRUE
+  d$OM_used[d$treatment%in% c("Farmers practice", "Farmers'practices")] <- FALSE
+   
 	d$geo_from_source <- FALSE
 
-	d$P_fertilizer <- d$K_fertilizer <-d$N_fertilizer <- as.numeric(NA)
-    d$fertilizer_type[d$fertilizer_used] <- "NPK"
-
+   d$P_fertilizer <- d$K_fertilizer <-d$N_fertilizer <- NA
+   
 	d$yield_part <- "grain"
 	d$yield_moisture <- as.numeric(NA)
-	d$yield_isfresh <- TRUE
+	d$yield_isfresh <- NA
 
 	carobiner::write_files(path, meta, d)
 }
