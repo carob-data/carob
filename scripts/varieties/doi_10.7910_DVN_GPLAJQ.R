2694c2f3ff6a4c00fe43b01184e8f4ffb39bb784
rhijmans <r.hijmans@gmail.com>
2026-07-09 11:49:36 -0700

    for #957

--- scripts/varieties/doi_10.7910_DVN_GPLAJQ.R
+++ scripts/varieties/doi_10.7910_DVN_GPLAJQ.R
@@ -16,10 +16,9 @@ Data on agronomic traits of maturity, plant height, grain yield and plant aspect
 	group <- "agronomy"
 	ff  <- carobiner::get_data(uri, path, group)
 
-
 	meta <- carobiner::get_metadata(uri, path, group, major=1, minor=0,
 		data_organization = "PURDUE",
-		publication = "NA",
+		publication = NA,
 		project = NA,
 		design = NA,
 		data_type = "on-farm experiment",
@@ -31,16 +30,15 @@ Data on agronomic traits of maturity, plant height, grain yield and plant aspect
 		carob_effort = 4
 	)
 	
-
 	f <- ff[basename(ff) == "Advanced drought tolerant sorghum hybrids at Kobo 2020.xlsx"]
 
 	r <- carobiner::read.excel(f)
 	d <- data.frame(
-	  country = "Ethiopia",
-	  adm2 = "North Wollo",
-	  location = r$Site,
-	  treatment = r$Genotype,
-	  rep = as.integer(r$Replicate),
+		country = "Ethiopia",
+		adm2 = "North Wollo",
+		location = r$Site,
+		treatment = r$Genotype,
+		rep = as.integer(r$Replicate),
 		yield = r$`Yield Kg/Ha`,
 		variety_pedigree = r$Pedigree,
 		plot_id = as.character(r$Plot),
@@ -50,27 +48,19 @@ Data on agronomic traits of maturity, plant height, grain yield and plant aspect
 		bird_damage = as.character(r$BirdDamage),
 		seed_weight = as.numeric(r$`1000GW`),
 		crop = "sorghum"
-)
+	)
 
-	d$trial_id <- "1"
-	
+	d$trial_id <- "1"	
 	d$on_farm <- NA
 	d$is_survey <- FALSE
 	d$irrigated <- NA
 	
-
 	d$longitude <- 39.643
 	d$latitude <- 12.1038
+	d$geo_from_source <- FALSE
 
-	  
-	d$geo_from_source <- TRUE
-
-	
-d$planting_date <- as.character(NA)
-d$harvest_date <- as.character(NA)
-
-
-
+	d$planting_date <- as.character(NA)
+	d$harvest_date <- as.character(NA)
 	d$P_fertilizer <- d$K_fertilizer <- d$N_fertilizer <- NA
 
 	d$yield_part <- "grain"
