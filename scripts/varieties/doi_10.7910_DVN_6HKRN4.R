680d79e9b5ec47b181044b4c22fb2d8a471f7a19
rhijmans <r.hijmans@gmail.com>
2026-07-10 08:22:41 -0700

    for #960

--- scripts/varieties/doi_10.7910_DVN_6HKRN4.R
+++ scripts/varieties/doi_10.7910_DVN_6HKRN4.R
@@ -28,18 +28,21 @@ Data on agronomic traits of maturity, plant height, grain yield and plant aspect
 		carob_contributor = "Premrose Masunungure",
 		carob_date = "2026-07-10",
 		carob_completion = 70,	
-		carob_effort = 5)
+		carob_effort = 5
+	)
 	
-
 	f <- ff[basename(ff) == "Advanced drought tolerant sorghum hybrids at Shiraro 2018.xlsx"]
-
 	r <- carobiner::read.excel(f)
 
 	d <- data.frame(
 	  country = "Ethiopia",
 	  location = r$Site,
-	  planting_date = as.character(as.Date(r$Sown)),
-	  treatment = r$Genotype,
+	  
+	  ### wrong conversion, that was later set th NA
+	  ### planting_date = as.character(as.Date(r$Sown))
+	  ### excel starts counting days from 1899-12-29 (!)
+	  planting_date = as.character(r$Sown + as.Date("1900-01-01") - 2),
+	  treatment = as.character(r$Entry),
 	  variety = r$Genotype,
 	  variety_pedigree = r$Pedigree,
 	  rep = as.integer(r$Replicate),
@@ -48,40 +51,38 @@ Data on agronomic traits of maturity, plant height, grain yield and plant aspect
 	  plant_height = r$PHTMean,
 	  flowering_days = r$DTF,
 	  maturity_days = r$DTM,
-	  str_co2 = as.numeric(r$`Striga count at 70 days`),
 	  crop = "sorghum"
-	  )
+	)
 
-	d$trial_id <- "1"
+	# all 7.5 m2
+	d$plot_area <- mean(10 * r$`Yield g/Plot` / r$`Yield Kg/Ha`)
+	d$plant_density <- 10000 * r$StandAtHarv  / d$plot_area
+	d$spike_density	<- 10000 * r$`Heads/Plot` / d$plot_area
+
+	# One would assume that the striga counts are per plot, in which case the per m2 striga density would be 
+	# s70 <- as.numeric(r$`Striga count at 70 days`) / d$plot_area
+	# s90 <- as.numeric(r$`Striga count at 90 days`) / d$plot_area
+    # but that seems way too high, so I am leaving them out 
 	
+	d$trial_id <- r$Type
 	d$on_farm <- NA
 	d$is_survey <- FALSE
 	d$irrigated <- NA
 	
-	#co-odinates from google, Sheraro not found in GADM
-	
 	d$longitude <- 37.773
 	d$latitude <-  14.396
+	# uncertainty from GADM adm3 = "Tahtay Adiyabo",
+	d$geo_uncertainty = 49096
+	d$geo_source = "Google Maps"
 	d$geo_from_source <- FALSE
 	
-	#adm3 = c("Tahtay Adiyabo"),
-	#longitude = c(37.7897),
-	#latitude = c(14.4267),
-	#geo_uncertainty = c(49096),
-	#geo_source = c("GADM 4.1, adm3")
-	
-	
-	
-	d$planting_date <- as.character(NA)
-	d$harvest_date <- as.character(NA)
-	
+	d$harvest_date <- as.character(NA)	
 	d$P_fertilizer <- d$K_fertilizer <- d$N_fertilizer <- NA
 	
 	d$yield_part <- "grain"
 	d$yield_moisture <- as.numeric(NA)
 	d$yield_isfresh <- NA
-	
-	
+
 	carobiner::write_files(path, meta, d)
 }
 
