ef7d255c880aadf3501c6dfee0bfcbbe27a32134
rhijmans <r.hijmans@gmail.com>
2026-07-13 09:19:04 -0700

    for #970

--- scripts/_rejected/doi_10.18174_odjar.v11i0.18784.R
+++ scripts/_rejected/doi_10.18174_odjar.v11i0.18784.R
@@ -2,7 +2,6 @@
 # license: GPL (>=3)
 
 ## ISSUES
-#1. Experiment has good useful data, but data is inaccessible ,"carobiner::get_data" can not access the files
 
 carob_script <- function(path) {
 
@@ -10,8 +9,9 @@ carob_script <- function(path) {
 "This paper presents data from a two-year study with sugar beet in Germany (2000 and 2001). A total of 27 field trials were conducted in a wide range of environmental conditions including trials with and without irrigation. Sequential harvests were made every 14-28 days between May and October. Root yield and quality, leaf yield, leaf area index and soil water content were determined in four replicates at each harvest date. Soil characteristics were assessed in the field and daily weather data were collected for each trial site. The dataset is suitable for validating sugar beet growth models."
 
 	uri <- "doi:10.18174/odjar.v11i0.18784"
-	group <- "rejected"
-	ff  <- carobiner::get_data(uri, path, group)
+	group <- "agronomy"
+	ff <- carobiner::get_data(uri, path, group, 
+			files="https://odjar.org/article/view/18784/18279/Dataset.zip")
 
 	meta <- carobiner::get_metadata(uri, path, group, major=1, minor=0,
 		data_organization = "Institute of Sugar Beet Research",
