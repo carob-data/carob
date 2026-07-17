feabdcb3e17fc9a359aa80035bdfb21fef0f3666
rhijmans <r.hijmans@gmail.com>
2026-07-17 10:39:09 -0700

    for #1004

--- scripts/agronomy/doi_10.25502_9g1e-ef44_d.R
+++ scripts/agronomy/doi_10.25502_9g1e-ef44_d.R
@@ -18,7 +18,7 @@
 
 carob_script <- function(path) {
   
-  "
+"
 Irrigation by fertilizer trial with cassava
 
 A factorial on-station cassava trial at IITA's Ibadan station, Nigeria,
@@ -37,39 +37,28 @@ depths each.
   ff  <- carobiner::get_data(uri, path, group)
   
   meta <- carobiner::get_metadata(uri, path, group, major=NA, minor=NA,
-                                  data_organization = "IITA",
-                                  publication = NA,
-                                  project = "AKILIMO",
-                                  design = "on-station factorial trial",
-                                  data_type = "on-station experiment",
-                                  treatment_vars = "irrigated;N_fertilizer;variety",
-                                  response_vars = "dmy_storage;dmy_total;harvest_index",
-                                  notes = NA,
-                                  carob_contributor = "Stella Muthoni",
-                                  carob_date = "2026-07-17",
-                                  carob_completion = 65,
-                                  carob_effort = 2
+		data_organization = "IITA",
+		publication = NA,
+		project = "AKILIMO",
+		design = "on-station factorial trial",
+		data_type = "on-station experiment",
+		treatment_vars = "irrigated;N_fertilizer;variety",
+		response_vars = "dmy_storage;dmy_total",
+		notes = NA,
+		carob_contributor = "Stella Muthoni",
+		carob_date = "2026-07-17",
+		carob_completion = 65,
+		carob_effort = 2
   )
   
   f1 <- ff[basename(ff) == "summaryyielddatairrigation4ckan.csv"]
-  f2 <- ff[basename(ff) == "data_dictionary_summaryyielddata.csv"]
+#  f2 <- ff[basename(ff) == "data_dictionary_summaryyielddata.csv"]
   f3 <- ff[basename(ff) == "soildata.csv"]
-  f4 <- ff[basename(ff) == "data_dictionary_soildata.csv"]
+#  f4 <- ff[basename(ff) == "data_dictionary_soildata.csv"]
   
   r1 <- read.csv(f1)
   r3 <- read.csv(f3)
-  ## r2/r4 are reference/dictionary files, not data - not used further.
-  
-  ### Fertilizer/irrigation/variety, derived into d1 (r1 itself untouched)
-  d1 <- data.frame(
-    N_fertilizer = ifelse(r1$Fert == "F0", 0, ifelse(r1$Fert == "F1", 75, NA)),
-    P_fertilizer = ifelse(r1$Fert == "F0", 0, ifelse(r1$Fert == "F1", 20, NA)),
-    K_fertilizer = ifelse(r1$Fert == "F0", 0, ifelse(r1$Fert == "F1", 90, NA)),
-    fertilizer_used = r1$Fert == "F1",
-    irrigated = r1$Irrigate == "I1",
-    variety = ifelse(r1$V == "V419", "TME 419", ifelse(r1$V == "V581", "TMS 0581", NA))
-  )
-  
+   
   ### Main table: dry matter biomass (t/ha -> kg/ha)
   d <- data.frame(
     trial_id = "1",
@@ -79,18 +68,16 @@ depths each.
     country = "Nigeria",
     adm1 = "Oyo",
     location = "Ibadan",
-    
     on_farm = FALSE,
     is_survey = FALSE,
-    
     crop = "cassava",
-    variety = d1$variety,
-    
-    irrigated = d1$irrigated,
-    N_fertilizer = d1$N_fertilizer,
-    P_fertilizer = d1$P_fertilizer,
-    K_fertilizer = d1$K_fertilizer,
-    fertilizer_used = d1$fertilizer_used,
+
+    N_fertilizer = ifelse(r1$Fert == "F0", 0, ifelse(r1$Fert == "F1", 75, NA)),
+    P_fertilizer = ifelse(r1$Fert == "F0", 0, ifelse(r1$Fert == "F1", 20, NA)),
+    K_fertilizer = ifelse(r1$Fert == "F0", 0, ifelse(r1$Fert == "F1", 90, NA)),
+    fertilizer_used = r1$Fert == "F1",
+    irrigated = r1$Irrigate == "I1",
+    variety = ifelse(r1$V == "V419", "TME 419", ifelse(r1$V == "V581", "TMS 0581", NA)),
     
     harvest_round = r1$H,   # suggested field, no actual dates given
     
@@ -103,14 +90,17 @@ depths each.
     dmy_stems = (r1$green_branch_DM + r1$Stem_branches_DM) * 1000,
     dmy_storage = (r1$OK_Root_DM + r1$bad_root_DM) * 1000,
     dmy_total = r1$TOTAL * 1000,
+
+    seed_rate = r1$Planting_stake_DM * 1000,  # DM not captured
     
-    harvest_index = r1$HI_OK_roots * 100,
-    
+	harvest_index = r1$HI_total,
+	## not that important as it is a derived value
+    #harvest_index_OKroots = r1$HI_OK_roots * 100,    
+	
     ## suggested fields, kept under their original names
-    green_branch_DM = r1$green_branch_DM * 1000,
-    Stem_branches_DM = r1$Stem_branches_DM * 1000,
-    Planting_stake_DM = r1$Planting_stake_DM * 1000,
-    HI_total = r1$HI_total * 100,
+    #green_branch_DM = r1$green_branch_DM * 1000,
+    #stem_branches_DM = r1$Stem_branches_DM * 1000,
+    
     
     longitude = 3.9470,
     latitude = 7.4419,
@@ -134,11 +124,13 @@ depths each.
     soil_sand = r3$Sand,
     soil_silt = r3$Silt,
     soil_clay = r3$Clay
-  )
+  )  
+  #d3$trial_id <- "1"
   
-  d3$trial_id <- 1
-  d3$trial_id <- as.character(d3$trial_id)
-  #d3$plot_id <- NULL
+  soil <- aggregate(d3[, c("soil_pH", "soil_C_total", "soil_N_total", "soil_P", "soil_K_exch", "soil_sand", "soil_silt", "soil_clay")],
+					d3[, c("depth_top", "depth_bottom")], mean)
   
-  carobiner::write_files(path, meta, d, d3)
+  soil <- data.frame(plot_id = rep(d$plot_id[!(d$plot_id %in% d3$plot_id)], each=2), soil)
+  
+  carobiner::write_files(path, meta, d, long=soil)
 }
