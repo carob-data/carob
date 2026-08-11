34272f12fdd03e7a708fda65b3c2c4808fc8c3d2
rhijmans <r.hijmans@gmail.com>
2026-08-11 05:27:09 -0700

    v

---
+++ passwords_template.R
@@ -0,0 +1,13 @@
+
+# to use this file, save a copy as "passwords.R"
+# and fill in your user name and password
+
+pwds <- function() {
+	list(
+		LSMS = c(username="user@email.com", password="I forgot again!"),
+		## DRYAD "Client ID" and "Secret" see https://datadryad.org/account 		
+		DRYAD = c(username="r33MAMbnIasdf088sdddss00___U", password="RTasdts--sadf999"),
+		DATAVERSE = c(name="user", password=NULL, institution="Department of Agriculture", position="researcher", email="user@agriculture.gov", answers=c("Carob project", "Research", "Other"))
+	)
+}
+
