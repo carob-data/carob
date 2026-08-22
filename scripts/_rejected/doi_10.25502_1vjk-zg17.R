c2bfee2afd43f992ab832bdbeef89be1df280454
rhijmans <r.hijmans@gmail.com>
2026-08-22 02:07:08 -0700

    for #1098

--- scripts/_rejected/doi_10.25502_1vjk-zg17.R
+++ scripts/_rejected/doi_10.25502_1vjk-zg17.R
@@ -3,7 +3,7 @@
 
 ## ISSUES
 
-#Rejected: Missing yield in raw data 
+#Rejected: No response data  
 
 carob_script <- function(path) {
 
@@ -14,7 +14,7 @@ Uniform Yield Trial (17 clones)  in Chitedze, Malawi  2016-17 Breeding Season.
 "
 
 	uri <- "doi:10.25502/1vjk-zg17"
-	group <- "varieties"
+	group <- "varieties_cassava"
 	ff  <- carobiner::get_data(uri, path, group)
 
 
@@ -33,33 +33,9 @@ Uniform Yield Trial (17 clones)  in Chitedze, Malawi  2016-17 Breeding Season.
 		carob_effort = 1
 	)
 	
-
-	f1 <- ff[basename(ff) == "2020-03-24t035828phenotype_download.csv"]
-	f2 <- ff[basename(ff) == "metadata.csv"]
-
-	r1 <- read.csv(f1)
-	r2 <- read.csv(f2)
-
-## process
-	
-	d1 <- data.frame(
-		year = r1$studyYear,
-		planting_date = as.Date(carobiner::eng_months_to_nr(r1$plantingDate), "%Y-%m-%d") ,
-		harvest_date = r1$harvestDate,
-		location = r1$locationName,
-		variety = r1$germplasmName,
-		trial_id = r1$observationUnitName,
-		rep = r1$replicate,
-		plot_id = r1$plotNumber,
-		longitude = 33.6371 ,
-		latitude = -13.981 ,
-		geo_source = "Google Maps",
-		country = "Malawi",
-		geo_from_source = FALSE,
-		yield = NA
-	)
-
-	carobiner::write_files(path, meta, d)
+	process_cassava <- carobiner::get_function("process_cassava", path, group)
+	d <- process_cassava(ff)
+	carobiner::write_files(path, meta, d$records, d$timerecs)
 }
 
 
