12c99b2683c9d13cb9ed521eb5354e72d68926f6
rhijmans <r.hijmans@gmail.com>
2026-08-24 07:32:36 -0700

    for #1094

--- scripts/varieties/doi_10.25502_76s6-2121_d.R
+++ scripts/varieties/doi_10.25502_76s6-2121_d.R
@@ -15,9 +15,6 @@ Advanced Variety Trials (AVT), Zambia - 2018
 Soybean (Glycine max (L.) Merrill.) is one of the most important oil crops of the world which also has tremendous importance as a food legume. The work on soybean aims at providing farmers, both commercial and subsistence, varieties with their preferred attributes to increase yield and income. These include high yield, resistance to deadly diseases, such as soybean rust, and insect pests, early maturity, good seed quality, and resistance to other stresses such as drought and soil acidity.The International Institute of Tropical Agriculture (IITA) is a key player in tropical soybean research and a partner of the Soybean Innovation Lab.
 "
 
-## when done, remove all the default comments, such as this one, from the script
-## only keep the comments you added that are specific to this dataset
-
 	uri <- "doi:10.25502/76s6-2121/d"
 	group <- "varieties"
 	ff  <- carobiner::get_data(uri, path, group)
@@ -30,14 +27,13 @@ Soybean (Glycine max (L.) Merrill.) is one of the most important oil crops of th
 		design = "Randomised Complete Block Design",
 		data_type = "on-farm experiment",
 		treatment_vars = "variety",
-		response_vars = "yield;seed_weight;plant_height;flowering_days;maturity_days;disease;severity_scale", 
+		response_vars = "yield;seed_weight;plant_height;flowering_days;maturity_days;disease", 
 		carob_contributor = "Illiana Kwenda",
 		carob_date = "2026-08-20",
 		carob_completion = 80,	
 		carob_effort = 3
 	)
 	
-
 	f1 <- ff[basename(ff) == "zambia-observation.csv"]
 	f2 <- ff[basename(ff) == "zambia-abbr.csv"]
 	#f3 <- ff[basename(ff) == "zambia-meta-data.csv"]
@@ -45,9 +41,8 @@ Soybean (Glycine max (L.) Merrill.) is one of the most important oil crops of th
 	r1 <- read.csv(f1)
 	r2 <- read.csv(f2)
 
-
 	d1 <- data.frame(
-	  location = as.character(r1$LOC),
+	  location = r1$LOC,
 	  trial_id = "1",
 	  plot_id = as.character(r1$ID),
 	  planting_date = "2018",
@@ -66,7 +61,7 @@ Soybean (Glycine max (L.) Merrill.) is one of the most important oil crops of th
 	  seed_weight = r1$SWT100 * 10,
 	  sl = r1$LODGING,
 	  dmy_residue = r1$BIOM,
-	  moist = r1$MC, # moisture content for grain yield
+	  yield_moisture = r1$MC, # moisture content for grain yield
 	  crop = "soybean",
 	  rust1 = r1$RUST_R3,
 	  rust2 = r1$RUST_R6,
@@ -78,87 +73,66 @@ Soybean (Glycine max (L.) Merrill.) is one of the most important oil crops of th
 	  record_id = as.integer(1:nrow(r1))
 	)
 	
-	d1$location <- paste0(
-	  toupper(substr(tolower(d1$location), 1, 1)),
-	  substr(tolower(d1$location), 2, nchar(d1$location))
-	)
-	
 	d1$plant_height[d1$plant_height %in% c(6, 167)] <- NA
 	
-	col <- names(d1)[grepl("Rust|frogeye|BB|BP|RED|CER|record_id", names(d1))]
-	long <- d1[, col]
+	cols <- names(d1)[grepl("rust|frogeye|BB|BP|RED|CER|record_id", names(d1))]
+	long <- d1[, cols]
 	
-	Nm <- names(long)[grepl("Rust|frogeye|BB|BP|RED|CER", names(long))]
+	cols <- cols[cols!="record_id"]
+	long <- reshape(long, varying = cols,  v.names = "disease_severity",  timevar = "disease", direction = "long")
 	
-	long <- reshape(
-	  long,
-	  varying = Nm,
-	  v.names = "disease_severity",
-	  timevar = "disease",
-	  direction = "long"
-	)
-	
-	long$disease <- c(
-	  "rust",
-	  "frogeye leaf spot",
-	  "bacterial blight",
-	  "bacterial pustule",
-	  "red leaf blotch",
-	  "cercospora leaf spot"
-	)[long$disease]
-	
+	long$disease <- c("rust", "frogeye leaf spot", "bacterial blight", "bacterial pustule",  "red leaf blotch", 
+						"cercospora leaf spot")[long$disease]
 	long <- long[!is.na(long$disease_severity), ]
+	long$disease_severity <- as.character(long$disease_severity)
+	long$id <- NULL
 
-	
-	severity_scale <- data.frame(
-	  disease = c(
-	    "rust",
-	    "frogeye leaf spot",
-	    "cercospora leaf spot",
-	    "bacterial blight",
-	    "bacterial pustule",
-	    "red leaf blotch"
-	  ),
-	  severity_scale = c(
-	    paste(range(c(r1$RUST_R3, r1$RUST_R6), na.rm = TRUE), collapse = "-"),
-	    paste(range(r1$FROGEYE, na.rm = TRUE), collapse = "-"),
-	    paste(range(r1$CERPOSPORA._LS, na.rm = TRUE), collapse = "-"),
-	    paste(range(r1$BB, na.rm = TRUE), collapse = "-"),
-	    paste(range(r1$BP_R3, na.rm = TRUE), collapse = "-"),
-	    paste(range(r1$RED_LB, na.rm = TRUE), collapse = "-")
-	  )
-	)
-	
-	long <- merge(long, severity_scale, by = "disease", all.x = TRUE)
+	d1 <- d1[, !(names(d1) %in% cols)]
+
+
+## this is not correct. you get 1-3, 1-4 and 1-5. 
+## that suggests to me that they are all 1-5. But it could be that the scale is 1-10, and that very high values 
+## are not observed.	
+#	severity_scale <- data.frame(
+#	  disease = c("rust", "frogeye leaf spot", "cercospora leaf spot",  "bacterial blight",  "bacterial pustule",  "red leaf blotch"),
+#	  severity_scale = c(
+#	    paste(range(c(r1$RUST_R3, r1$RUST_R6), na.rm = TRUE), collapse = "-"),
+#	    paste(range(r1$FROGEYE, na.rm = TRUE), collapse = "-"),
+#	    paste(range(r1$CERPOSPORA._LS, na.rm = TRUE), collapse = "-"),
+#	    paste(range(r1$BB, na.rm = TRUE), collapse = "-"),
+#	    paste(range(r1$BP_R3, na.rm = TRUE), collapse = "-"),
+#	    paste(range(r1$RED_LB, na.rm = TRUE), collapse = "-")
+#	  )
+#	)
+#	long <- merge(long, severity_scale, by = "disease", all.x = TRUE)
 	
 ## see carobiner::geocode
 ###IITA-SARAH and SEEDCO are found in Lusaka district whereas Good Nature Agro is located in Chipata district
-##The geo-coordinates where taken from google maps. geo_uncertanity were obatined from adm2 = Chipata and Lusaka districts
+##The coordinates where taken from google maps. 
+
+## geo_uncertanity were obatined from adm2 = Chipata and Lusaka districts
+## RH:: this uncertainty is much too large if you know where these locations are 
 	
 	geo <- data.frame(
-	  adm1 = c("Eastern", "Lusaka", "Lusaka"), 
-	  adm2 = c("Chipata", "Lusaka", "Lusaka"), 
-	  location = c("Good nature agro", "Iita-sarah", "Seedco"), 
+      location = c("IITA-SARAH", "Good Nature Agro", "SEEDCO"), 
 	  longitude = c(32.6450, 28.18173, 28.26166), 
 	  latitude = c(-13.6450, -15.1809, -15.455833), 
-	  geo_uncertainty = c(38917, 19447, 19447), 
-	  geo_source = rep("GADM 4.1, adm2", 3),
+	  geo_uncertainty = c(1000, 1000, 1000), 
+	  geo_source = "Google Maps",
 	  geo_from_source = FALSE
 	)
 	
 	d <- merge(d1, geo, by = "location", all.x = TRUE)
 	
-  d$P_fertilizer <- d$K_fertilizer <- d$N_fertilizer <- d$S_fertilizer <- as.numeric(NA) 
-  d$fertilizer_type <- NA
-
+	d$P_fertilizer <- d$K_fertilizer <- d$N_fertilizer <- as.numeric(NA) 
   
-  d$country = "Zambia"
-  d$on_farm <- TRUE
-  d$is_survey <- FALSE 
-  d$irrigated <- NA
+	d$country = "Zambia"
+	d$on_farm <- TRUE
+	d$is_survey <- FALSE 
+	d$irrigated <- NA
 	d$yield_part <- "grain"
 	d$yield_isfresh <- TRUE
-	d$yield_moisture <- 13
+	## d$yield_moisture <- 13 from where?
 
 	carobiner::write_files(path, meta, d, long=long)
 }
