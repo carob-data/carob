07129a44f0f6f26d7877ccd8869b65e93c07c31d
Robert Hijmans <r.hijmans@gmail.com>
2026-09-16 08:05:52 -0700

    Update doi_10.5281_zenodo.19929127.R

--- scripts/agronomy/doi_10.5281_zenodo.19929127.R
+++ scripts/agronomy/doi_10.5281_zenodo.19929127.R
@@ -2,19 +2,18 @@
 # license: GPL (>=3)
 
 ## NOTES
-# 26 measured parameters across a 3-season maize biochar/basanite trial;
+# 26 measured variables across a 3-season maize biochar/basanite trial;
 # Selected only: Yield, root biomass, bulk density, and Striga
 # Root Biomass and Soil Bulk Density were measured once for the whole trial
 
 # Suggested new terms"
-#                      str_co4-8 (different weeks of striga count)
-#                      amend_rate
-#                      ammendment_type
+#  str_co4-8 (different weeks of striga count)
+#  amend_rate
+#  ammendment_type
 
 ## ISSUES
-# No coordinates anywhere in source - Location only says Siaya Kenya
 # id: plot_id(s) do not match between long and wide records
-#                   caused by in-situ pH, which has 9 replicates per treatment (vs 3 everywhere else in the trial).
+#   caused by in-situ pH, which has 9 replicates per treatment (vs 3 everywhere else in the trial).
 
 
 carob_script <- function(path) {
@@ -23,11 +22,11 @@ carob_script <- function(path) {
 Data for: Effect of biochar and basanite on maize yield,
 fertilizer efficiency and phosphorus sorption in a tropical Oxisol (PyMiCCS Kenya field trial)
 
-# Field trial and in-vitro batch experiment data &mdash; Effect of biochar and 
+# Field trial and in-vitro batch experiment data; Effect of biochar and 
 basanite on maize yield, fertilizer efficiency and phosphorus sorption in a tropical Oxisol &nbsp; 
 This dataset contains the raw experimental data underlying the manuscript 'Effect of biochar and basanite on maize yield,
 fertilizer efficiency and phosphorus sorption in a tropical Oxisol' by Meyer zu Drewer, Roobroeck, Maritim, Lotz, Kammann, 
-Hartmann, Broszat, Schmidt, and Hagemann. &nbsp; 
+Hartmann, Broszat, Schmidt, and Hagemann.; 
 The data were collected during a three-season maize field trial on an Oxisol in Siaya, western Kenya (2023 - 2025), 
 and during a complementary six-week in-vitro phosphorus sorption batch experiment.
 "
@@ -37,18 +36,18 @@ and during a complementary six-week in-vitro phosphorus sorption batch experimen
 	ff  <- carobiner::get_data(uri, path, group)
 
 	meta <- carobiner::get_metadata(uri, path, group, major=6, minor=NA,
-	                                data_organization = "Ithaka Institute",
-	                                publication = NA,
-	                                project = "PyMiCCS",
-	                                design = "9 treatments (controls, biochar, basanite, co-application), 3 replicates, 3 seasons, Siaya, Kenya, 2023-2025.",
-	                                data_type = NA,   # on-farm vs on-station not stated in source - not guessed
-	                                treatment_vars = "treatment; N_fertilizer; P_fertilizer; K_fertilizer",
-	                                response_vars = "yield; dmy_residue",
-	                                notes = NA,
-	                                carob_contributor = "Stella Muthoni",
-	                                carob_date = "2026-08-30",
-	                                carob_completion = 75,
-	                                carob_effort = 3
+	          data_organization = "Ithaka Institute",
+	          publication = NA,
+	          project = "PyMiCCS",
+	          design = "9 treatments (controls, biochar, basanite, co-application)",
+	          data_type = NA,   # on-farm vs on-station not stated in source - not guessed
+	          treatment_vars = "treatment; N_fertilizer; P_fertilizer; K_fertilizer",
+	          response_vars = "yield; dmy_residue",
+	          notes = NA,
+	          carob_contributor = "Stella Muthoni",
+	          carob_date = "2026-08-30",
+	          carob_completion = 75,
+	          carob_effort = 3
 	)
 	
 	f1 <- ff[basename(ff) == "Data_Meyer_zu_Drewer_Kenya.csv"]          # field trial, long format
@@ -60,48 +59,41 @@ and during a complementary six-week in-vitro phosphorus sorption batch experimen
 	
 	# Create the yield trial database
 	## season 1 N:P:K rates (kg/ha) - season 2-3 differs for the amended treatments (P/K rise from 0/0 to 10/20)
-	trt_s1 <- data.frame(
+	trt <- data.frame(
 	  treatment = c("Zero Control","Control","PK-half","PK-full","RE-BC_5","RE-BC_10","Co-Ap_10","BC_5","RP_5"),
-	  N = c(0,90,90,90,90,90,90,90,90),
-	  P = c(0,0,10,20,0,0,0,0,0),
-	  K = c(0,0,20,40,0,0,0,0,0),
-	  fertilizer_type = "unknown",   
-	  amendment_type_ = c(NA,NA,NA,NA,"rock-enhanced biochar","rock-enhanced biochar","biochar+basanite co-application","biochar","basanite (rock powder)"),
-	  amend_rate = c(NA,NA,NA,NA,5,10,10,5,5),
+	  N_fertilizer = c(0,90,90,90,90,90,90,90,90),
+	  P_fertilizer = c(0,0,10,20,0,0,0,0,0),
+	  K_fertilizer = c(0,0,20,40,0,0,0,0,0),
+#	  amendment_type = c(NA,NA,NA,NA,"rock-enhanced biochar","rock-enhanced biochar","biochar+basanite co-application","biochar","basanite (rock powder)"),
+#	  amend_rate = c(NA,NA,NA,NA,5,10,10,5,5),
+# is this correct?
+      amendment_type = c("none","none","none","none","rock-enhanced biochar","rock-enhanced biochar","biochar+basanite co-application","biochar","basanite (rock powder)"),
+	  amend_rate = c(0,0,0,0,5,10,10,5,5),
 	  OM_used = c(FALSE,FALSE,FALSE,FALSE,TRUE,TRUE,TRUE,TRUE,FALSE)   # TRUE only for biochar-containing treatments
 	)
-	names(trt_s1)[names(trt_s1) %in% c("N","P","K")] <- c("N_fertilizer","P_fertilizer","K_fertilizer")
-	trt_s1$OM_amount <- ifelse(trt_s1$OM_used, trt_s1$amend_rate * 1000, NA)   # t/ha -> kg/ha
+	trt$OM_amount <- ifelse(trt$OM_used, trt$amend_rate * 1000, NA)   # kg/ha
 	
-	trt_s23 <- trt_s1
-	trt_s23$P_fertilizer[5:9] <- 10
-	trt_s23$K_fertilizer[5:9] <- 20
-	
-	# Select relevant carob terminag per season
-	d1_s1 <- merge(
-	  setNames(r1[r1$parameter == "Grain Yield 1st season t ha", c("treatment","replicate","value")], c("treatment","replicate","yield")),
-	  setNames(r1[r1$parameter == "Stover Yield 1st season t ha", c("treatment","replicate","value")], c("treatment","replicate","dmy_residue")),
-	  by = c("treatment","replicate"), all = TRUE)
-	d1_s1 <- merge(d1_s1, trt_s1, by="treatment", all.x=TRUE)
-	d1_s1$trial_id <- "1"
-	
-	d1_s2 <- merge(
-	  setNames(r1[r1$parameter == "Grain Yield 2nd season t ha", c("treatment","replicate","value")], c("treatment","replicate","yield")),
-	  setNames(r1[r1$parameter == "Stover Yield 2nd season t ha", c("treatment","replicate","value")], c("treatment","replicate","dmy_residue")),
-	  by = c("treatment","replicate"), all = TRUE)
-	d1_s2 <- merge(d1_s2, trt_s23, by="treatment", all.x=TRUE)
-	d1_s2$trial_id <- "2"
-	
-	d1_s3 <- merge(
-	  setNames(r1[r1$parameter == "Grain Yield 3rd season t ha", c("treatment","replicate","value")], c("treatment","replicate","yield")),
-	  setNames(r1[r1$parameter == "Stover Yield 3rd season t ha", c("treatment","replicate","value")], c("treatment","replicate","dmy_residue")),
-	  by = c("treatment","replicate"), all = TRUE)
-	d1_s3 <- merge(d1_s3, trt_s23, by="treatment", all.x=TRUE)
-	d1_s3$trial_id <- "3"
-	
-	d1 <- rbind(d1_s1, d1_s2, d1_s3)
-	d1$yield <- d1$yield * 1000
-	d1$dmy_residue <- d1$dmy_residue * 1000
+	trt2 <- trt
+	trt2$P_fertilizer[5:9] <- 10
+	trt2$K_fertilizer[5:9] <- 20
+	trt <- rbind(data.frame(season=1, trt), data.frame(season="2", trt2), data.frame(season="3", trt2))
+
+	# get season 
+	r1$season <- gsub("\\D", "", r1$parameter)
+
+	# make wide
+	s <- r1[grepl("season", r1$parameter) & !grepl("Soil pH|Striga", r1$parameter), ]
+	s$parameter <- gsub(" 1st season| 2nd season| 3rd season", "", s$parameter)
+	s <- reshape(s, timevar = "parameter",  idvar = c("season", "treatment", "subgroup", "replicate"),  direction = "wide")
+
+	d1 <- data.frame(
+		treatment = s$treatment, 
+		replicate = s$replicate,
+		season = s$season,
+		yield = s$`value.Grain Yield t ha` * 1000, 
+		dmy_residue = s$`value.Stover Yield t ha`  * 1000
+	)
+    d1 <- merge(d1, trt, by=c("treatment", "season"), all.x=TRUE)
 	
 	#Bulk density - Measured once 
 	bd <- r1[r1$parameter == "Soil Bulk Density g cm3", c("treatment","replicate","value")]
@@ -109,13 +101,17 @@ and during a complementary six-week in-vitro phosphorus sorption batch experimen
 	d1 <- merge(d1, bd, by=c("treatment","replicate"), all.x=TRUE)
 	
 	# Roots biomass
+	## need to be for a particular season??
 	roots <- r1[r1$parameter == "Root Biomass g dw", c("treatment","replicate","value")]
 	names(roots)[3] <- "dw_roots"
 	d1 <- merge(d1, roots, by=c("treatment","replicate"), all.x=TRUE)
 	
 	## Extract the striga data
 	## Already defined in terminag: Week 8 = str_co1, 10 weeks = str_co2, 3 weeks = str_co3
-	## Suggested for this data: "Week 2"="str_co4", "Week 4"="str_co5", "Week 6"="str_co6", "Week 12"="str_co7", "Week 14"="str_co8"
+	## that is old stuff from maize varieties that will be removed soon, from before we had long 
+	## variables. Make this a long variable with time in DAP?
+
+	## Suggested for this data: "Week 2"="str_co4", "Week 4"="str_co5", "Week 6"="str_co6", "Week 12"="str_co7", "Week 14"="str_co8" 
 	striga_s2 <- r1[r1$parameter == "Striga count season 2", c("treatment","replicate","subgroup","value")]
 	names(striga_s2) <- c("treatment","replicate","week","striga_count")
 	striga_s2$trial_id <- "2"
@@ -140,22 +136,13 @@ and during a complementary six-week in-vitro phosphorus sorption batch experimen
 	d1$crop <- "maize"
 	d1$country <- "Kenya"
 	d1$adm1 <- "Siaya"
-	
 	## Georeference Siaya county (adm1) per https://carob-data.org/contribute/georeference.html
-	geo <- data.frame(
-	  adm1 = c("Siaya"),
-	  longitude = c(34.2488),
-	  latitude = c(-0.0546),
-	  geo_uncertainty = c(44931),
-	  geo_source = c("GADM 4.1, adm1")
-	)
-	
+	d1$longitude = 34.2488
+	d1$latitude = -0.0546
+	d1$geo_uncertainty = 44931
+	d1$geo_source = "GADM 4.1, adm1"
 	d1$geo_from_source <- FALSE
-	d1$latitude <- geo$latitude
-	d1$longitude <- geo$longitude
-	d1$geo_uncertainty <- geo$geo_uncertainty
-	d1$geo_source <- geo$geo_source
-	
+		
 	d1$is_survey <- FALSE
 	d1$on_farm <- TRUE
 	d1$irrigated <- FALSE
