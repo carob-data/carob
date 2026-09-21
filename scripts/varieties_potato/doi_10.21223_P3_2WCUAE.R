c7bddee9fdb2f8225bd03146966ab95b3ac11610
maryammohamed-lang <maryam.mohamed@aims.ac.rw>
2026-09-21 13:43:38 +0200

    Update doi_10.21223_P3_2WCUAE.R

--- scripts/varieties_potato/doi_10.21223_P3_2WCUAE.R
+++ scripts/varieties_potato/doi_10.21223_P3_2WCUAE.R
@@ -12,9 +12,7 @@ carob_script <- function(path) {
   
   ff <- carobiner::get_data(uri, path, group)
   
-  meta <- carobiner::get_metadata(
-    uri, path, group,
-    major = 2, minor = 0,
+  meta <- carobiner::get_metadata(uri, path, group, major = 2, minor = 0,
     data_organization = "CIP",
     publication = NA,
     project = NA,
